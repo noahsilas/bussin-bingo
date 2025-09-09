b2545f654cd4070ce602277b6b0bb3d0ffd3c6ed
Noah Silas <noahking@gmail.com>
2025-09-08 19:32:46 -0700

    Add ability to mark spots as visited
    I'm just storing this info in local storage, which means that it doesn't
    persist across devices but avoids any backend requirements. Nice.
    
    Click the "visited" button again toggles the visited state off.

--- public/script.js
+++ public/script.js
@@ -4,11 +4,35 @@ let map;
 let openWindow = null;
 const markers = {};
 
-function h2(text) {
-  const el = document.createElement('h2');
-  el.textContent = text;
+const visited = JSON.parse(localStorage.getItem('visited') ?? '{}');
+const visit = (name, status = true) => {
+  visited[name] = status;
+  markers[name].forEach((marker) => {
+    if (status) {
+      marker.element.classList.add('visitedMarker');
+    } else {
+      marker.element.classList.remove('visitedMarker');
+    }
+  })
+  localStorage.setItem('visited', JSON.stringify(visited));
+}
+
+function tag(tag, children) {
+  const el = document.createElement(tag);
+  if (typeof children == 'string') {
+    el.textContent = children;
+  } else {
+    for (const child of children) {
+      el.appendChild(child);
+    }
+  }
   return el;
 }
+
+const h2 = (children) => tag('h2', children);
+const div = (children) => tag('div', children);
+const p = (children) => tag('p', children);
+const button = (children) => tag('button', children);
 
 async function initMap() {
   const mapP = google.maps.importLibrary("maps");
@@ -31,10 +55,17 @@ async function initMap() {
         position: { lat: address.location.lat, lng: address.location.lng },
         title: place.name,
       });
+      if (place.name in visited && visited[place.name]) {
+        marker.element.classList.add('visitedMarker');
+      }
       markers[place.name].push(marker);
+      const b = button('Visited');
+      b.addEventListener('click', () => {
+        visit(place.name, !visited[place.name])
+      })
       const infoWindow = new google.maps.InfoWindow({
         headerContent: h2(place.name),
-        content: `<p>${address.formatted_address}</p>`,
+        content: div([p(address.formatted_address), b])
       });
       marker.content.addEventListener('mouseenter', () => {
         markers[place.name].forEach((marker) => {
