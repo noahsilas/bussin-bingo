ccfca635cb1d3bea3519b196e6bd58ca0711caf5
Noah Silas <noahking@gmail.com>
2025-09-10 10:03:58 -0700

    Reformat places JSON
    Take advantage of nice JS-style objects. I originally had this in a
    standalone `.json` file where these quotes are needed, but in a `.js`
    file we can omit them!

--- geocode.js
+++ geocode.js
@@ -3,27 +3,27 @@ const fs = require('node:fs')
 // List of participating locations
 // via https://www.sfmta.com/calendar/munis-bussin-bingo
 const places = [
-  { "name": "The Laundromat", "addresses": ["3725 Balboa Street"] },
-  { "name": "Jim's", "addresses": ["2420 Mission Street"] },
-  { "name": "M Stop Market & Deli", "addresses": ["2598 San Jose Avenue"] },
-  { "name": "Sunset Cantina", "addresses": ["3414 Judah Street"] },
-  { "name": "Motoring Coffee", "addresses": ["1525 Union Street"] },
-  { "name": "New Love Tea", "addresses": ["601 Broadway"] },
-  { "name": "Whack Donuts", "addresses": ["4 Embarcadero Center Suite #4507"] },
-  { "name": "Smitten Ice Cream", "addresses": ["904 Valencia Street"] },
-  { "name": "Devil's Teeth Baking Company", "addresses": ["3876 Noriega Street", "3619 Balboa Street", "1 Embarcadero Center"] },
-  { "name": "Thorough Bread & Pastry", "addresses": ["248 Church Street"] },
-  { "name": "Breadbelly", "addresses": ["1408 Clement Street", "1070 Maryland Street (Pier 70 Building 12)"] },
-  { "name": "The French Spot", "addresses": ["1042 Larkin Street"] },
-  { "name": "Fillmore Bakeshop", "addresses": ["1890 Fillmore Street"] },
-  { "name": "Outta Sight Pizza", "addresses": ["422 Larkin Street", "643 Clay Street"] },
-  { "name": "Standard Deviant Brewing", "addresses": ["280 14th Street", "1070 Maryland Street (Pier 70 Building 12)"] },
-  { "name": "Peacekeeper", "addresses": ["925 Bush Street"] },
-  { "name": "Zeitgeist", "addresses": ["199 Valencia Street"] },
-  { "name": "Robberbaron", "addresses": ["2032 Polk Street"] },
-  { "name": "Propagation", "addresses": ["895 Post Street"] },
-  { "name": "Rikki's", "addresses": ["2223 Market Street"] },
-  { "name": "Oasis Fusion Kitchen", "addresses": ["4826 Mission Street"] }
+  { name: "The Laundromat", addresses: ["3725 Balboa Street"] },
+  { name: "Jim's", addresses: ["2420 Mission Street"] },
+  { name: "M Stop Market & Deli", addresses: ["2598 San Jose Avenue"] },
+  { name: "Sunset Cantina", addresses: ["3414 Judah Street"] },
+  { name: "Motoring Coffee", addresses: ["1525 Union Street"] },
+  { name: "New Love Tea", addresses: ["601 Broadway"] },
+  { name: "Whack Donuts", addresses: ["4 Embarcadero Center Suite #4507"] },
+  { name: "Smitten Ice Cream", addresses: ["904 Valencia Street"] },
+  { name: "Devil's Teeth Baking Company", addresses: ["3876 Noriega Street", "3619 Balboa Street", "1 Embarcadero Center"] },
+  { name: "Thorough Bread & Pastry", addresses: ["248 Church Street"] },
+  { name: "Breadbelly", addresses: ["1408 Clement Street", "1070 Maryland Street (Pier 70 Building 12)"] },
+  { name: "The French Spot", addresses: ["1042 Larkin Street"] },
+  { name: "Fillmore Bakeshop", addresses: ["1890 Fillmore Street"] },
+  { name: "Outta Sight Pizza", addresses: ["422 Larkin Street", "643 Clay Street"] },
+  { name: "Standard Deviant Brewing", addresses: ["280 14th Street", "1070 Maryland Street (Pier 70 Building 12)"] },
+  { name: "Peacekeeper", addresses: ["925 Bush Street"] },
+  { name: "Zeitgeist", addresses: ["199 Valencia Street"] },
+  { name: "Robberbaron", addresses: ["2032 Polk Street"] },
+  { name: "Propagation", addresses: ["895 Post Street"] },
+  { name: "Rikki's", addresses: ["2223 Market Street"] },
+  { name: "Oasis Fusion Kitchen", addresses: ["4826 Mission Street"] }
 ]
 
 async function geocode(address) {
